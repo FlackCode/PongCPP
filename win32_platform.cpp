f78392a8b29ec762f97bf0e75f49e88041f0b774
flack <163582560+FlackCode@users.noreply.github.com>
2024-07-10 17:04:34 +0300

    Drawing shapes, organised code.

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,11 +1,25 @@
+#include "utils.cpp"
 #include <Windows.h>
 
-bool running = true;
+global_variable bool running = true;
 
-void* buffer_memory;
-int buffer_width;
-int buffer_height;
-BITMAPINFO buffer_bitmap_info;
+//cleanup time
+
+struct Render_State {
+    int height, width;
+    void* memory;
+
+    BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
+
+//void* memory;
+//int width;
+//int height;
+//BITMAPINFO bitmap_info;
 
 LRESULT CALLBACK windowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     LRESULT result = 0;
@@ -18,23 +32,23 @@ LRESULT CALLBACK windowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
         case WM_SIZE: {
             RECT rect;
             GetClientRect(hwnd, &rect);
-            buffer_width = rect.right - rect.left;
-            buffer_height = rect.bottom - rect.top;
+            render_state.width = rect.right - rect.left;
+            render_state.height = rect.bottom - rect.top;
             //creating the buffer size
-            int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+            int buffer_size = render_state.width * render_state.height * sizeof(unsigned int);
             
             // if the buffer memory is already allocated but we resize the window, 
             // free the memory and reallocate the memory depending on the window size
-            if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
+            if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
             //allocating memory to the buffer, allowing to read and write to it.
-            buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            render_state.memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-            buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
-            buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
-            buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
-            buffer_bitmap_info.bmiHeader.biPlanes = 1;
-            buffer_bitmap_info.bmiHeader.biBitCount = 32;
-            buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+            render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+            render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+            render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+            render_state.bitmap_info.bmiHeader.biPlanes = 1;
+            render_state.bitmap_info.bmiHeader.biBitCount = 32;
+            render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
 
 
         }
@@ -80,16 +94,14 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
         //Simulate
         // we got black as our default color so now we iterate through every w/h pixel.
         // create a pixel pointer and then paint it to orange
-        unsigned int* pixel = (unsigned int*)buffer_memory;
-        for (int y = 0; y < buffer_height; y++) {
-            for (int x = 0; x < buffer_width; x++) {
-                *pixel++ = 0xff00ff*x + 0x00ff00*y;
-            }
-        }
+        clear_screen(0xffffff);
+        draw_rect(0, 0, 10, 10, 0xff0000);
+        draw_rect(30, 30, 5, 5, 0xff0000);
+        draw_rect(-20, 20, 8, 3, 0xff0000);
 
         //Render
-        StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, 
-            buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+        StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, 
+            render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
     }
 
 }
